da10eaea46f23b071a9a9467cb763a5447cc3e1d
Shoaib Ejaz <shoaibejazabc@gmail.com>
2020-04-06 20:27:56 +0500

    Updates

---
+++ precache-manifest.0fc4533c3ddc8246259c5b482533cc4f.js
@@ -0,0 +1,186 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77c4b746374801c2b8a112edb5585d79",
+    "url": "/erpv1/index.html"
+  },
+  {
+    "revision": "6056f30a444a55469fd2",
+    "url": "/erpv1/static/css/main.85a3690f.chunk.css"
+  },
+  {
+    "revision": "4497c186a1daf6efa66a",
+    "url": "/erpv1/static/js/2.e5f96aa1.chunk.js"
+  },
+  {
+    "revision": "e145179326161a5c9ba58a4075d305c5",
+    "url": "/erpv1/static/js/2.e5f96aa1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6056f30a444a55469fd2",
+    "url": "/erpv1/static/js/main.62f56cf3.chunk.js"
+  },
+  {
+    "revision": "aca1603eef2aafbac5c7",
+    "url": "/erpv1/static/js/runtime-main.76388879.js"
+  },
+  {
+    "revision": "ca82bc743710e8f84904d035d92bf5cd",
+    "url": "/erpv1/static/media/25243.ca82bc74.svg"
+  },
+  {
+    "revision": "6a0f34a00b75b6a9bc9eb950240f915c",
+    "url": "/erpv1/static/media/NoPath - Copy (10).6a0f34a0.png"
+  },
+  {
+    "revision": "8cf002e283dd1deeaec8d6dad34d4885",
+    "url": "/erpv1/static/media/NoPath - Copy (11).8cf002e2.png"
+  },
+  {
+    "revision": "b31ce692bc47440ad1ab9f717923778a",
+    "url": "/erpv1/static/media/address.b31ce692.svg"
+  },
+  {
+    "revision": "37ef9aa925ff98234643b6e30f824c6b",
+    "url": "/erpv1/static/media/alarm-clock.37ef9aa9.svg"
+  },
+  {
+    "revision": "aa922b9a44fd3421002f8beaf817f62e",
+    "url": "/erpv1/static/media/blackplus.aa922b9a.svg"
+  },
+  {
+    "revision": "f7bac801fbe3f5a93e6bb9769ea1ed58",
+    "url": "/erpv1/static/media/cancel.f7bac801.svg"
+  },
+  {
+    "revision": "2e959dff594a9872e33ba77497358be2",
+    "url": "/erpv1/static/media/close.2e959dff.svg"
+  },
+  {
+    "revision": "1d51e9545e83d4ef04f46976842ff3d6",
+    "url": "/erpv1/static/media/company.1d51e954.svg"
+  },
+  {
+    "revision": "b02a178b676fadf1b7853a494a972253",
+    "url": "/erpv1/static/media/dashboard.b02a178b.svg"
+  },
+  {
+    "revision": "15cdc1e21f4bff10485c3d55702a0325",
+    "url": "/erpv1/static/media/delete.15cdc1e2.svg"
+  },
+  {
+    "revision": "c5ed17aafb97913595d7cabb731fc6bb",
+    "url": "/erpv1/static/media/delete.c5ed17aa.svg"
+  },
+  {
+    "revision": "a5f9967ddcc9f4e6087d9d9aeeee2636",
+    "url": "/erpv1/static/media/docs.a5f9967d.svg"
+  },
+  {
+    "revision": "6bbba73686edd11264405ba5da0b6642",
+    "url": "/erpv1/static/media/down-arrow.6bbba736.svg"
+  },
+  {
+    "revision": "f5f9acc7789727035a1e19d6705c5ba5",
+    "url": "/erpv1/static/media/down-grey.f5f9acc7.svg"
+  },
+  {
+    "revision": "7aec5cb4b275f003122b65bc2cb6c815",
+    "url": "/erpv1/static/media/down-white.7aec5cb4.svg"
+  },
+  {
+    "revision": "181abc578441aaae2932ed40158c8ae7",
+    "url": "/erpv1/static/media/down.181abc57.svg"
+  },
+  {
+    "revision": "bdf2954c66080a9b9afc42c3ee4977d4",
+    "url": "/erpv1/static/media/edit.bdf2954c.svg"
+  },
+  {
+    "revision": "5915a630e9e4df04f07a6edc623f5663",
+    "url": "/erpv1/static/media/expenses.5915a630.svg"
+  },
+  {
+    "revision": "ad588631ed12d8ff3dddc145a001fa18",
+    "url": "/erpv1/static/media/flags.ad588631.svg"
+  },
+  {
+    "revision": "e4f78bf78cbb1b22b0b6fd4535faac38",
+    "url": "/erpv1/static/media/idea.e4f78bf7.svg"
+  },
+  {
+    "revision": "71437644cf6e34a184386af3f48a120c",
+    "url": "/erpv1/static/media/invoice.71437644.svg"
+  },
+  {
+    "revision": "fd954cfabad42b26f678ea7de9e507b1",
+    "url": "/erpv1/static/media/invoice.fd954cfa.svg"
+  },
+  {
+    "revision": "696127f9a0bca4ac0d64e383eb803380",
+    "url": "/erpv1/static/media/mail.696127f9.svg"
+  },
+  {
+    "revision": "b3de483576e821eb5dfe3c32ff1941d2",
+    "url": "/erpv1/static/media/male.b3de4835.svg"
+  },
+  {
+    "revision": "34f3f57e6cc37a84b9457942a5e0cb9b",
+    "url": "/erpv1/static/media/menu.34f3f57e.svg"
+  },
+  {
+    "revision": "a450cc103b020589bf97577c53c009d7",
+    "url": "/erpv1/static/media/menu.a450cc10.svg"
+  },
+  {
+    "revision": "bfc53b6a6bee1a0b135dddd79dd8468d",
+    "url": "/erpv1/static/media/menu.bfc53b6a.svg"
+  },
+  {
+    "revision": "845daac9c59a5c7b4898e9664f7a557f",
+    "url": "/erpv1/static/media/minus.845daac9.svg"
+  },
+  {
+    "revision": "24b4b901ffa9dad42617cdee18eb1130",
+    "url": "/erpv1/static/media/plus.24b4b901.svg"
+  },
+  {
+    "revision": "511f5b800c4bac7262d035429a108687",
+    "url": "/erpv1/static/media/plus.511f5b80.svg"
+  },
+  {
+    "revision": "95f7a38f6b8678ca3669a71335972c39",
+    "url": "/erpv1/static/media/plus.95f7a38f.svg"
+  },
+  {
+    "revision": "9f4b63d3217f8d41cc9721644585509e",
+    "url": "/erpv1/static/media/pone.9f4b63d3.svg"
+  },
+  {
+    "revision": "06e87ca7e4910d7c428991ab00286882",
+    "url": "/erpv1/static/media/pound.06e87ca7.svg"
+  },
+  {
+    "revision": "f256828f087af4233d747f9c807a7a75",
+    "url": "/erpv1/static/media/preview.f256828f.svg"
+  },
+  {
+    "revision": "dff122b6f9cf4b9595f2fe9bddd6dcf6",
+    "url": "/erpv1/static/media/profit-loss.dff122b6.svg"
+  },
+  {
+    "revision": "730e2e1245dce38293e4eafcb72dd418",
+    "url": "/erpv1/static/media/right-arrow.730e2e12.svg"
+  },
+  {
+    "revision": "6ac2accbd9b03235e36eb4acd5cb97eb",
+    "url": "/erpv1/static/media/stack.6ac2accb.svg"
+  },
+  {
+    "revision": "544b176199a1fe6e4cfbbd4104c54523",
+    "url": "/erpv1/static/media/sun.544b1761.svg"
+  },
+  {
+    "revision": "42f99957f8c178d0b02cbfc3b91c9c9e",
+    "url": "/erpv1/static/media/text.42f99957.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/erpv1/precache-manifest.0fc4533c3ddc8246259c5b482533cc4f.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/erpv1/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
